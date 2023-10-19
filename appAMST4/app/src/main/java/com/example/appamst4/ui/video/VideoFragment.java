36d313b5dbf8a74733f54cc0f12bd9ad6ecd53b3
glorona <glorona@espol.edu.ec>
2023-10-18 23:57:17 -0500

    Desafio video view

--- appAMST4/app/src/main/java/com/example/appamst4/ui/video/VideoFragment.java
+++ appAMST4/app/src/main/java/com/example/appamst4/ui/video/VideoFragment.java
@@ -1,7 +1,11 @@
 package com.example.appamst4.ui.video;
 
+
+import androidx.annotation.RequiresApi;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,7 +15,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import com.example.appamst4.R;
 
@@ -22,6 +28,7 @@ public class VideoFragment extends Fragment {
 
     private FragmentVideoBinding binding;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         VideoViewModel slideshowViewModel =
@@ -30,8 +37,17 @@ public class VideoFragment extends Fragment {
         binding = FragmentVideoBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textVideo;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //final TextView textView = binding.textVideo;
+        //slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        final VideoView videoView = binding.videoView;
+        String videoPath = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.iphone;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(getActivity());
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
         return root;
     }
 
